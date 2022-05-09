67c40e67ac5d6a06aaa6ea8361bfd76adb74e073
陶真 <toumakoto@MacBook-Pro-14-inch-2074.local>
2022-05-08 22:39:05 -0400

    edit query_result and delete redundant wrapper files

--- amrqa/query_result.py
+++ amrqa/query_result.py
@@ -1,35 +1,50 @@
+"""This module computes the results of sparql queries from DBpedia and saves the results in a json file.
+Examples:
+    $ python query_result.py --queryfile generated_queries.json --savefile result.json
+    
+"""
+
 from SPARQLWrapper import SPARQLWrapper, JSON
 import json
 import sys
+import argparse
 
 
-endpoint = 'https://dbpedia.org/sparql'
-sp = SPARQLWrapper(endpoint)
+def get_queries(input_file, out_file):
 
-#read data file
-file_name = sys.argv[1]
-with open(file_name, 'r') as f:
-	data = json.load(f)
+	endpoint = 'https://dbpedia.org/sparql'
+	sp = SPARQLWrapper(endpoint)
+	with open(input_file, 'r') as f:
+		data = json.load(f)
 
-result = []
-for k,e in data.items():
-	# print(e["query"]["sparql"])
-	try:
-		statement = e["sparql"]
-		sp.setQuery(statement)
-		sp.setReturnFormat(JSON)
-		r = sp.query().convert()
-		r["status"] = "success"
-	except:
-		r = {}
-		r["status"] = "fail"
-	r["id"] = k
-	result.append(r)
+	result = []
+	for k,e in data.items():
+		try:
+			statement = e["sparql"]
+			sp.setQuery(statement)
+			sp.setReturnFormat(JSON)
+			r = sp.query().convert()
+			r["status"] = "success"
+		except:
+			r = {}
+			r["status"] = "fail"
+		r["id"] = k
+		result.append(r)
+	with open(out_file, "w") as f2:
+		return json.dump(result, f2)
 
+def main(args):
+	get_queries(args.queryfile, args.savefile)
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--queryfile',
+                        help='Filepath where the sparql'
+                        'query are stored.')
+    parser.add_argument('--savefile',
+                        help='Filepath where the query results'
+                        'will be stored.')
+    args = parser.parse_args()
+    main(args)
 
-
-out_file = open(sys.argv[2], "w")
-json.dump(result, out_file)
-out_file.close()
 
